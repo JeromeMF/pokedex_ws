bd4bf7815932c2e26f3a3042b63375d70005dc94
Jerome <jerome@Darunia.local>
2016-09-07 21:30:59 +0100

    Building the Pokemon list page and respective components

--- client/app/components/pokemonlist/pokemonlist.component.js
+++ client/app/components/pokemonlist/pokemonlist.component.js
@@ -2,11 +2,14 @@ import template from './pokemonlist.html';
 import controller from './pokemonlist.controller';
 import './pokemonlist.sass';
 
+
 let pokemonlistComponent = {
-  restrict: 'E',
-  bindings: {},
-  template,
-  controller
+	restrict: 'E',
+	bindings: {},
+	template,
+	controller
 };
 
+
 export default pokemonlistComponent;
+
